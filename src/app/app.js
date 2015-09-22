8a99bad652bedd178da83f48d382a4776283d819
tommymei <tommy@sendence.com>
2015-09-22 03:39:43 -0400

    updated README

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,12 @@
 'use strict';
 
 angular.module('inspinia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider) {
     $stateProvider
         .state('main', {
             url: "/main",
             templateUrl: "app/main/main.html",
             data: { pageTitle: 'Main' }
         })
-    $urlRouterProvider.otherwise('/index/main');
   })
 ;
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,11 +1,31 @@
 'use strict';
 
 angular.module('inspinia')
-  .controller('MainCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+  .controller('MainCtrl', ['$scope', '$http', '$window', '$interval', function ($scope, $http, $window,$interval) {
     // TODO: make this configureable 
     var server = "localhost:8080";
     // default 
+    $scope.lastFetchTime = undefined;
     $scope.isFetchMode = true;
+
+    $scope.setFetchMode = function() {
+      if ($scope.lastFetchTime === undefined) {
+        $scope.isFetchMode = true;
+        return;
+      }
+
+      var d = new Date();
+      var currentTime = d.getTime();
+
+      // 15 seconds elapse
+      if (currentTime - $scope.lastFetchTime >= 15000) {
+        $scope.isFetchMode = true;
+        return;
+      }
+
+      $scope.isFetchMode = false;
+      return;
+    }
 
     $scope.getFetchLabel = function () {
       return $scope.isFetchMode ? 'Fetch - Click Me!' : 'Fetch - Disabled';
@@ -15,6 +35,7 @@ angular.module('inspinia')
       return !$scope.isFetchMode ? 'Set - Click Me!' : 'Set - Disabled';
     }
 
+    // ----- Fetch Function call
     $scope.fetch = function () {
       var getRes = $http.get('http://' + server + '/backend/queue/fetch');
       getRes.success(function (data) {
@@ -26,13 +47,15 @@ angular.module('inspinia')
         $scope.red = data.red;
         $scope.blue = data.blue;
         $scope.green = data.green;
-        $scope.isFetchMode = false;
+        $scope.lastFetchTime = new Date().getTime();
+        $scope.setFetchMode();
       });
       getRes.error(function (data) {
         alert('error on fetch');
       });
     }
 
+    // --- Set Function call
     $scope.set = function () {
 
       if ($scope.id === undefined) {
@@ -45,8 +68,13 @@ angular.module('inspinia')
           throw 'data is undefined';
         }
         if (data.response === 'success') {
-          $scope.isFetchMode = true;
+          $scope.lastFetchTime = undefined;
+          $scope.setFetchMode();
           $window.document.body.style.backgroundColor = 'rgb(' + $scope.red + ',' + $scope.green + ',' + $scope.blue + ')';
+        } else {
+          // need this because someone else might have grabbed it, client and server are not in lock step of the 15 seconds
+          // rare case but it could happen.
+          alert('ooops, you either did not set in time or something went wrong');
         }
       });
       getRes.error(function (data) {
@@ -54,5 +82,8 @@ angular.module('inspinia')
       });
     }
 
-    /*    $interval(function() {       $scope.message = {         time: $window.moment().format('MMMM Do YYYY, h:mm:ss a')       };     }, 1000);*/
+    // recompute fetch mode every 1 second
+    $interval(function() {
+         $scope.setFetchMode();
+    }, 1000);
   }]);
